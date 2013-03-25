04e39dd6b6019ab0584db3fe119ce2ee2c3dc0d5
Klaus Kämpf <kkaempf@suse.de>
2013-03-25 10:45:16 +0100

    Hide Helper, move to ProviderTesting methods

---
+++ lib/provider-testing.rb
@@ -0,0 +1,22 @@
+#
+# provider-testing.rb
+#
+# A Ruby-based testsuite for CIM providers
+#
+# Copyright (c) 2013 Klaus Kämpf <kkaempf@suse.de>
+#
+# Licensed under the Ruby license
+#
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+module ProviderTesting
+  VERSION = '1.4'
+  require 'provider-testing/helper'
+  def self.setup *args
+    Helper.setup args
+  end
+  def self.teardown
+    Helper.teardown
+  end
+end
--- lib/provider-testing/version.rb
+++
@@ -1,3 +0,0 @@
-module ProviderTesting
-  VERSION = '0.1.0'
-end
