a5d74cffdfb06ddaf0fdbc069a27ccaaecef2056
Klaus Kämpf <kkaempf@suse.de>
2013-09-18 15:04:03 +0200

    Factor out reading of the .registration file

--- lib/provider-testing/mkreg.rb
+++ lib/provider-testing/mkreg.rb
@@ -3,23 +3,32 @@
 #   sfcb-specific .reg format
 #
 module ProviderTesting
-def self.mkreg from, out
-#  STDERR.puts "mkreg #{from}"
+# read .registration file, return array of
+#  0         1         2            3              4..-1
+#  classname namespace providername providermodule caps
+def self.rdreg from
   File.open(from, "r") do |f|
     while (l = f.gets)
       next if l =~ /^\s*#.*$/
       l.chomp!
       next if l.empty?
-      #  0         1         2            3              4..-1
-      #  classname namespace providername providermodule caps
       reg = l.split(" ")
       raise unless reg.size > 4
-      out.puts "[#{reg[0]}]"
-      out.puts "  provider: #{reg[2]}"
-      out.puts "  location: #{reg[3]}"
-      out.puts "  type: #{reg[4..-1].join(' ')}"
-      out.puts "  namespace: #{reg[1]}"
+      yield reg
     end
+  end
+end
+
+def self.mkreg from, out
+#  STDERR.puts "mkreg #{from}"
+  self.rdreg from do |reg|
+    #  0         1         2            3              4..-1
+    #  classname namespace providername providermodule caps
+    out.puts "[#{reg[0]}]"
+    out.puts "  provider: #{reg[2]}"
+    out.puts "  location: #{reg[3]}"
+    out.puts "  type: #{reg[4..-1].join(' ')}"
+    out.puts "  namespace: #{reg[1]}"
   end
 end
 
