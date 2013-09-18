c5edcea557aab0acc29d599a5559ee50fadbfe45
Klaus Kämpf <kkaempf@suse.de>
2013-09-18 15:05:11 +0200

    bump version to 0.3.1

--- lib/provider-testing.rb
+++ lib/provider-testing.rb
@@ -11,7 +11,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module ProviderTesting
-  VERSION = '0.3.0'
+  VERSION = '0.3.1'
   require 'provider-testing/helper'
   def self.setup klass, namespace = "test/test"
     Helper.setup klass, namespace
